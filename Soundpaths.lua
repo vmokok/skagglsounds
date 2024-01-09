180f49fc9545c3716da24f6174c877f9dc0b8ca0
vmokok <blizzobninsk@gmail.com>
2024-01-09 09:02:17 +0600

    first commit

---
+++ Soundpaths.lua
@@ -0,0 +1,53 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- -----
+-- BACKGROUND
+-- -----
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFF00FF00Skagglsok: Rocalo-Drum|r", [[Interface\Addons\SharedMedia_Skagglsound\sound\drum.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: Rocalo-wrong|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\wrong.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: Backup please!|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\backup-naxyu.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: Are you laughing at me?!|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\are-you-fucking-rofling.ogg]])
+
+LSM:Register("sound", "|cFF00FF00Skagglsok: blin2|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\blin2.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: konchene|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\konchene.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: mark|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\mark.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: masturbation|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\masturbation.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: prikalivaetes|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\prikalivaetes.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: prostopizdec|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\prostopizdec.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: run_better|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\run_better.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: syebali|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\syebali.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: syebaliblyat|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\syebaliblyat.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: syebalinaxyu|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\syebalinaxyu.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: trash|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\trash.ogg]])
+LSM:Register("sound", "|cFF00FF00Skagglsok: xylevistoite|r",
+    [[Interface\Addons\SharedMedia_Skagglsound\sound\xylevistoite.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
